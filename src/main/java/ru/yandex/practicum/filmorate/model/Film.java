7b2c2e5b092fc2d44e1e07bef52872e7bb908e2c
Mishcore <mishcore.retranslator@gmail.com>
2023-05-24 08:54:05 +1000

    version: ТЗ-9

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,34 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+import ru.yandex.practicum.filmorate.validator.ValidReleaseDate;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private static final String CINEMA_BIRTH_DATE = "28.12.1895";
+
+    @Positive
+    private Integer id;
+
+    @NotBlank
+    private String name;
+
+    @Size(max = 200)
+    private String description;
+
+    @NotNull
+    @ValidReleaseDate(earliestDate = CINEMA_BIRTH_DATE)
+    private LocalDate releaseDate;
+
+    @Positive
+    private int duration;
+
+    public Film(String name, String description, LocalDate releaseDate, int duration) {
+        this.name = name;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.duration = duration;
+    }
+}
